50e376499d3f97023b135f6af479d405a592b87e
khoitq <khoitq>
2022-04-22 16:25:17 +0700

    fix export jar main class not found

--- assets/export-jar/declare.sh
+++ assets/export-jar/declare.sh
@@ -1,28 +1,28 @@
 #project name quy định tên thư mục lib, xml, jar 
-export project_name=aladin
+export project_name=shinobibot
 # export project_name=tradingsystemweb
 
 
 #project dir là thư mục chứa source code
-export project_dir=/Volumes/home/Project/aladin/git/aladin/aladin
+export project_dir=/Volumes/home/Project/shinobibot/git/shinobibot/shinobibot
 # export project_dir=/Volumes/home/Project/tradingsystemweb/git/tradingsystemweb
 
 #main class là đường dẫn đến class Main
-export main_class=com.shinobiweb.main.Main
+export main_class=com.shinobibot.main.ShinobiBotMain
 # export main_class=com.tradingsystemweb.main.TradingSystemWebMain
 
 
 #Thư mục chứa file xml kết quả
-export output_dir=/Volumes/home/Project/tets/test-output
+export output_dir=/Volumes/home/Project/shinobibot/deploy
 # export output_dir=/Volumes/home/Project/ant-build-generation/git/ant-build-generation/out
 
 
 #workspace là folder chứa workspace của project
-export workspace=/Volumes/home/Project/aladin/git/aladin/aladin
+export workspace=/Volumes/home/Project/workspace
 # export workspace=/Volumes/home/Project/workspace
 
 
 #quy định đường dẫn đến các file jar được import ngoài maven nếu không có thì để là () - xóa chuỗi phía trong
-declare -a project_jar_directory_dependencies=("/Volumes/home/Project/aladin/git/aladin/aladin/lib/shinobiserver.jar")
+declare -a project_jar_directory_dependencies=("/Volumes/home/Project/tradingsystemweb/git/tradingsystemweb/lib/shinobiserver.jar")
 # declare -a project_jar_directory_dependencies=("/Volumes/home/Project/aladin/git/aladin/aladin/lib/shinobiserver.jar")
 
--- assets/export-jar/export-xml.sh
+++ assets/export-jar/export-xml.sh
@@ -11,8 +11,7 @@ export xml_file_name=build_${project_name}.xml
 rm $output_dir/$project_name.jar
 brew install maven
 
-
-
+brew install ant
 
 #create dependency list file 
 #maven jar
@@ -47,6 +46,17 @@ sed -i -e 's/\${{main_class}}/'${main_class}'/g' $output_dir/$xml_file_name
 sed -i -e "s|\${{workspace}}|${workspace}|g" $output_dir/$xml_file_name
 sed -i -e "s|\${{jar_file_in_lib_folder_class_path}}|${jar_file_in_lib_folder_class_path}|g" $output_dir/$xml_file_name
 sed -i -e "s|\${{jar_file_in_user_maven_folder_class_path}}|${jar_file_in_user_maven_folder_class_path}|g" $output_dir/$xml_file_name
+sed -i -e "s|\${{project_dir}}|${project_dir}|g" $output_dir/$xml_file_name
+
+
+
+
+cd $output_dir
+
+ant -f $xml_file_name
+
+
+echo "done"
 
 
 rm $output_dir/$xml_file_name-e
@@ -54,16 +64,3 @@ rm $output_dir/export-jar-file.command
 rm $output_dir/template.xml
 rm $output_dir/$dependency_list_file_name
 rm $output_dir/$dependency_list_file_name-e
-
-
-
-echo "done"
-
-brew install ant
-
-cd $output_dir
-
-ant -f $xml_file_name
-
-
-osascript -e 'tell application "Terminal" to quit'
